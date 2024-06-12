dd090d91ae806243d691593a9b314a88d2deb112
bilongue237 <tchafla100@gmail.com>
2024-06-12 17:38:42 -0500

    simplified python banking program

--- main.py
+++ main.py
@@ -1,51 +1,30 @@
-def show_balance():
-    print(f"Your balance is ${balance:.2f}")
+from operation import show_balance,deposit,withdraw
 
-def deposit():
-    amount =  float(int(input('Amount: ')))
+def main():
+    balance = 0
+    is_running= True
 
-    if amount < 0:
-            print("Not a valid amount")
-            return 0
-    else:
-         return amount
+    while is_running:
+        print("Banking Program")
+        print("1.Show Balance")
+        print('2.Deposit')
+        print('3.Withdraw')
+        print('4.Exit')
 
+        choice = input('Enter your choice(1-4): ')
 
-def withdraw():
-    amount = float(int(input('Amount: ')))
+        if choice == "1":
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -=withdraw(balance)
+        elif choice == '4':
+            is_running = False
+        else:
+            print('Not valid')
 
-    if  amount > balance:
-        print("Not enough funds")
-        return 0
-    elif amount < 0:
-        print("amount must be greater thant 0")
-        return 0
+    print("Thank you and have a nice day!")
 
-    else:
-        return amount
-
-
-balance = 0
-is_running= True
-
-while is_running:
-    print("Banking Program")
-    print("1.Show Balance")
-    print('2.Deposit')
-    print('3.Withdraw')
-    print('4.Exit')
-
-    choice = input('Enter your choice(1-4): ')
-
-    if choice == "1":
-        show_balance()
-    elif choice == '2':
-        balance += deposit()
-    elif choice == '3':
-        balance -=withdraw()
-    elif choice == '4':
-        is_running = False
-    else:
-        print('Not valid')
-
-print("Thank you and have a nice day!")
+if __name__ == "__main__":
+    main()
